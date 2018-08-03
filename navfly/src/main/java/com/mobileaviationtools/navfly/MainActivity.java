8e2969ca97ddbe26c73f10a7d0d363762617c535
Mobile Aviation Tools <rob@robenanita.nl>
2018-08-03 12:46:55 +0200

    Added initial code and permissions to manifest

--- navfly/src/main/java/com/mobileaviationtools/navfly/MainActivity.java
+++ navfly/src/main/java/com/mobileaviationtools/navfly/MainActivity.java
@@ -3,11 +3,121 @@ package com.mobileaviationtools.navfly;
 import android.app.Activity;
 import android.os.Bundle;
 
+import org.oscim.android.MapPreferences;
+import org.oscim.android.MapView;
+import org.oscim.android.cache.TileCache;
+import org.oscim.backend.CanvasAdapter;
+import org.oscim.core.MapPosition;
+import org.oscim.layers.GroupLayer;
+import org.oscim.layers.tile.buildings.BuildingLayer;
+import org.oscim.layers.tile.vector.VectorTileLayer;
+import org.oscim.layers.tile.vector.labeling.LabelLayer;
+import org.oscim.map.Map;
+import org.oscim.renderer.BitmapRenderer;
+import org.oscim.renderer.GLViewport;
+import org.oscim.scalebar.DefaultMapScaleBar;
+import org.oscim.scalebar.ImperialUnitAdapter;
+import org.oscim.scalebar.MapScaleBar;
+import org.oscim.scalebar.MapScaleBarLayer;
+import org.oscim.scalebar.MetricUnitAdapter;
+import org.oscim.theme.VtmThemes;
+import org.oscim.tiling.TileSource;
+import org.oscim.tiling.source.OkHttpEngine;
+import org.oscim.tiling.source.oscimap4.OSciMap4TileSource;
+
 public class MainActivity extends Activity {
+    final static boolean USE_CACHE = false;
+
+    MapView mMapView;
+    Map mMap;
+    MapPreferences mPrefs;
+    VectorTileLayer mBaseLayer;
+    private TileCache mCache;
+    TileSource mTileSource;
+
+    private DefaultMapScaleBar mapScaleBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMapView = (MapView) findViewById(R.id.mapView);
+        mMap = mMapView.map();
+        mPrefs = new MapPreferences(MainActivity.class.getName(), this);
+
+        setupMap();
+        createLayers();
+    }
+
+    void setupMap()
+    {
+        mTileSource = OSciMap4TileSource.builder()
+                .httpFactory(new OkHttpEngine.OkHttpFactory())
+                .build();
+
+        if (USE_CACHE) {
+            mCache = new TileCache(this, null, "tile.db");
+            mCache.setCacheSize(512 * (1 << 10));
+            mTileSource.setCache(mCache);
+        }
+        mBaseLayer = mMap.setBaseMap(mTileSource);
+
+        /* set initial position on first run */
+        MapPosition pos = new MapPosition();
+        mMap.getMapPosition(pos);
+        if (pos.x == 0.5 && pos.y == 0.5)
+            mMap.setMapPosition(53.08, 8.83, Math.pow(2, 16));
+    }
+
+    void createLayers() {
+        GroupLayer groupLayer = new GroupLayer(mMap);
+        groupLayer.layers.add(new BuildingLayer(mMap, mBaseLayer));
+        groupLayer.layers.add(new LabelLayer(mMap, mBaseLayer));
+        mMap.layers().add(groupLayer);
+
+        mapScaleBar = new DefaultMapScaleBar(mMap);
+        mapScaleBar.setScaleBarMode(DefaultMapScaleBar.ScaleBarMode.BOTH);
+        mapScaleBar.setDistanceUnitAdapter(MetricUnitAdapter.INSTANCE);
+        mapScaleBar.setSecondaryDistanceUnitAdapter(ImperialUnitAdapter.INSTANCE);
+        mapScaleBar.setScaleBarPosition(MapScaleBar.ScaleBarPosition.BOTTOM_LEFT);
+
+        MapScaleBarLayer mapScaleBarLayer = new MapScaleBarLayer(mMap, mapScaleBar);
+        BitmapRenderer renderer = mapScaleBarLayer.getRenderer();
+        renderer.setPosition(GLViewport.Position.BOTTOM_LEFT);
+        renderer.setOffset(5 * CanvasAdapter.getScale(), 0);
+        mMap.layers().add(mapScaleBarLayer);
+
+        mMap.setTheme(VtmThemes.DEFAULT);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mPrefs.load(mMapView.map());
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mPrefs.save(mMapView.map());
+        mMapView.onPause();
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mapScaleBar != null)
+            mapScaleBar.destroy();
+
+        if (mCache != null)
+            mCache.dispose();
+
+        mMapView.onDestroy();
+
+        super.onDestroy();
     }
 }
