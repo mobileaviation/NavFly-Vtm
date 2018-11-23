a9b00841339d1aa0055a0f76d724b85869859232
Mobile Aviation Tools <rob@robenanita.nl>
2018-11-23 17:18:37 +0100

    Work on dashboard

--- nav-fly/src/main/java/com/mobileaviationtools/nav_fly/Dashboard/DashboardFragment.java
+++ nav-fly/src/main/java/com/mobileaviationtools/nav_fly/Dashboard/DashboardFragment.java
@@ -6,8 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.mobileaviationtools.nav_fly.Location.FspLocation;
 import com.mobileaviationtools.nav_fly.R;
+
+import org.w3c.dom.Text;
 
 
 public class DashboardFragment extends Fragment {
@@ -27,6 +31,18 @@ public class DashboardFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_dashboard, container, false);
         return view;
+    }
+
+    public void setLocation(FspLocation location)
+    {
+        TextView toDestTextView = (TextView) view.findViewById(R.id.toDestTextView);
+        TextView coarseTextView = (TextView) view.findViewById(R.id.coarseTextView);
+        TextView speedTextView = (TextView) view.findViewById(R.id.speedTextView);
+        TextView heightTextView = (TextView) view.findViewById(R.id.heightTextView);
+        TextView qnhStationTextView = (TextView) view.findViewById(R.id.qnhStationTextView);
+        TextView eetTextView = (TextView) view.findViewById(R.id.eetTextView);
+        TextView zuluTimeTestView = (TextView) view.findViewById(R.id.zuluTimeTestView);
+
     }
 
 }
